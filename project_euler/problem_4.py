57892b9314fecdfe38cb9e6cef136774ca139b01
tabibyte <tabib.hasanzadeh@gmail.com>
2022-03-12 21:19:25 +0400

    problem 3

---
+++ project_euler/problem_4.py
@@ -0,0 +1,12 @@
+number = 999
+palindromebig = 0
+while number > 99:
+    for i in range(99,999):
+        palindrome = number*i
+        tostring = str(palindrome)
+        reverse = int(tostring[::-1])
+        if palindrome == reverse:
+            if palindrome > palindromebig:
+                palindromebig = palindrome
+    number=number-1
+print(palindromebig)
