2034e0b52edb6527820024f5217646c6b23ded88
Tabib Hasanzada <56648986+tabibyte@users.noreply.github.com>
2022-03-16 23:52:49 +0400

    Update problem_4.py

--- project_euler/problem_4.py
+++ project_euler/problem_4.py
@@ -1,8 +1,10 @@
 # finding biggest palindrome
 number = 999
 palindromebig = 0
+# checks all first and second three digit multipliers
 while number > 99:
     for i in range(99,999):
+        # checks if palindrome
         palindrome = number*i
         tostring = str(palindrome)
         reverse = int(tostring[::-1])
